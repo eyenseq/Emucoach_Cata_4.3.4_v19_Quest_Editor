80a308be4622fbf753aab60b311470907e3c5ccf
Eyens <68038631+eyenseq@users.noreply.github.com>
2026-01-12 06:11:21 -0500

    Update DBC_DIR path and add AREATABLE_DBC

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 # Change this 
-DBC_DIR = Path(r"C:\Wows\CATAGOLD\Repack\dbc")
+DBC_DIR = Path(r"C:\PATH_TO_YOUR\dbc")
 
 # Optional: per-file overrides
 SKILLLINE_DBC = DBC_DIR / "SkillLine.dbc"
 SPELL_DBC     = DBC_DIR / "Spell.dbc"
+
 AREATABLE_DBC = DBC_DIR / "AreaTable.dbc"
