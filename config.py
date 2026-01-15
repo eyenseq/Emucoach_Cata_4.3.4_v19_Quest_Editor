62729636f154e7bfe643b20b9185fafc70c5e646
Eyens <68038631+eyenseq@users.noreply.github.com>
2026-01-15 03:37:19 -0500

    Add QUESTSORT_DBC to config.py

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ DBC_DIR = Path(r"C:\PATH_TO_YOUR\dbc")
 # Optional: per-file overrides
 SKILLLINE_DBC = DBC_DIR / "SkillLine.dbc"
 SPELL_DBC     = DBC_DIR / "Spell.dbc"
-
 AREATABLE_DBC = DBC_DIR / "AreaTable.dbc"
+QUESTSORT_DBC = DBC_DIR / "QuestSort.dbc"
+
