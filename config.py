b35fa9936286d2b9bff2705c8af6a7cd18389791
Eyens <68038631+eyenseq@users.noreply.github.com>
2026-01-15 16:36:36 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 # Change this 
-DBC_DIR = Path(r"C:\Wows\CATAGOLD\Repack\dbc")
+DBC_DIR = Path(r"C:\path_to_your\dbc")
 
 # Optional: per-file overrides
 SKILLLINE_DBC = DBC_DIR / "SkillLine.dbc"
 SPELL_DBC     = DBC_DIR / "Spell.dbc"
 AREATABLE_DBC = DBC_DIR / "AreaTable.dbc"
+
 QUESTSORT_DBC = DBC_DIR / "QuestSort.dbc"
