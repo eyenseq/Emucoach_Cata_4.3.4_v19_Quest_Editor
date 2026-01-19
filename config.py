ded8c391f01bb2751862c32bbed69dcb8afd660f
Eyens <68038631+eyenseq@users.noreply.github.com>
2026-01-18 23:57:01 -0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Change this 
-DBC_DIR = Path(r"C:\path_to_your\dbc")
+DBC_DIR = Path(r"C:\Path_To_Your\dbc")
 
 # Optional: per-file overrides
 SKILLLINE_DBC = DBC_DIR / "SkillLine.dbc"
@@ -9,3 +9,7 @@ SPELL_DBC     = DBC_DIR / "Spell.dbc"
 AREATABLE_DBC = DBC_DIR / "AreaTable.dbc"
 
 QUESTSORT_DBC = DBC_DIR / "QuestSort.dbc"
+
+# Extra DBCs used by pickers
+FACTION_DBC       = DBC_DIR / "Faction.dbc"
+CURRENCYTYPES_DBC = DBC_DIR / "CurrencyTypes.dbc"
